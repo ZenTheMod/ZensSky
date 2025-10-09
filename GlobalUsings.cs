5d0171685a8cde59d805ae92ec9d14d694a5095e
ZenTheMod <88523932+ZenTheMod@users.noreply.github.com>
2025-10-08 19:46:35 -0700

    new sourcegen prog

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,19 +2,19 @@
 global using Color = Microsoft.Xna.Framework.Color;
 global using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
-    // Generated Namespaces.
+    // Asset Namespaces.
 global using CompatEffects = ZensSky.Assets.Effects.Compat;
 global using SkyEffects = ZensSky.Assets.Effects.Sky;
 global using UIEffects = ZensSky.Assets.Effects.UI;
 
 global using Models = ZensSky.Assets.Models;
 
-global using SkyTextures = ZensSky.Assets.Textures.Sky.Textures;
-global using FablesTextures = ZensSky.Assets.Textures.Sky.FablesMoons.Textures;
-global using StarTextures = ZensSky.Assets.Textures.Sky.StarStyles.Textures;
+global using SkyTextures = ZensSky.GeneratedAssets.Textures.Sky.Textures;
+global using FablesTextures = ZensSky.GeneratedAssets.Textures.Sky.FablesMoons.Textures;
+global using StarTextures = ZensSky.GeneratedAssets.Textures.Sky.StarStyles.Textures;
 
-global using UITextures = ZensSky.Assets.Textures.UI.Textures;
-global using ButtonTextures = ZensSky.Assets.Textures.UI.Buttons.Textures;
-global using PanelTextures = ZensSky.Assets.Textures.UI.PanelStyle.Textures;
+global using UITextures = ZensSky.GeneratedAssets.Textures.UI.Textures;
+global using ButtonTextures = ZensSky.GeneratedAssets.Textures.UI.Buttons.Textures;
+global using PanelTextures = ZensSky.GeneratedAssets.Textures.UI.PanelStyle.Textures;
 
-global using MiscTextures = ZensSky.Assets.Textures.Textures;
+global using MiscTextures = ZensSky.GeneratedAssets.Textures.Textures;
